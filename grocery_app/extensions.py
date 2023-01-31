4fbc592b4cc00df9bb82bf040ce83328d540fc4c
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2023-01-30 21:09:12 -0700

    added signup and login templates

--- grocery_app/extensions.py
+++ grocery_app/extensions.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from grocery_app.config import Config
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
@@ -17,3 +18,11 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
