d219aaa940c55847b58390852b08450dc51ebdc1
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2023-01-30 19:56:19 -0700

    initial commit

--- grocery_app/extensions.py
+++ grocery_app/extensions.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from grocery_app.config import Config
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+
+###########################
+# Authentication
+###########################
+
+# TODO: Add authentication setup code here!
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
